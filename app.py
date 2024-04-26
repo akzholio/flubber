24b864e9100981097797034223bc0e02466e13eb
Akzhol Imangaliyev <akzhol.imangaliyev@yougov.com>
2024-04-26 13:13:11 +0200

    Add more functionality, set custom theme and wide mode.

--- app.py
+++ app.py
@@ -2,21 +2,28 @@ import streamlit as st
 import cv2
 import pytesseract
 
+
+def wide_space_default():
+    st.set_page_config(layout="wide")
+
+
+wide_space_default()
+
 img = st.file_uploader("Choose a file", type=["jpg", "png"])
 
 
 if img is not None:
     filebytes = img.getvalue()
     thresh = st.slider("Threshold", 0, 255, 127)
-    st.write("Threshold", thresh)
     maxval = st.slider("Maxval", 0, 255, 255)
-    st.write("Maxval", maxval)
+    thresh_types = {"THRESH_BINARY": 0, "THRESH_BINARY_INV": 1}
+    thresh_type = st.selectbox("Threshold type", thresh_types.keys())
 
     with open(img.name, 'wb') as f:
         f.write(filebytes)
     image = cv2.imread(img.name)
     im_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    _, im_bw = cv2.threshold(im_gray, thresh, maxval, cv2.THRESH_BINARY_INV)
+    _, im_bw = cv2.threshold(im_gray, thresh, maxval, thresh_types[thresh_type])
 
     col1, col2 = st.columns(2)
     with col1:
